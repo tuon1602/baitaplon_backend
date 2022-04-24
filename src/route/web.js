2f070afee1384f1ad6a7b67355e2853195aa7818
tuon1602 <tuan69878@gmail.com>
2022-04-24 17:17:34 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,14 @@
+import express from "express";
+import homeController from "../controller/homeController"
+
+let router = express.Router();
+
+let initWebRoutes = (app) =>
+{
+    router.get('/',homeController.getHomePage)
+    router.get('/about',homeController.getAboutPage)
+    //rest api method : get
+    return app.use("/",router)
+}
+
+module.exports = initWebRoutes;
---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser"
+import viewEngine from "./config/viewEngine"
+import initWebRoutes from "./route/web"
+import connectDB from "./config/connectDB"
+require('dotenv').config()
+
+let app = express()
+
+//config app
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+let port = process.env.PORT || 6969
+
+app.listen(port, () =>
+{
+    console.log("backend nodejs is running on the port:" + port)
+})
+
+viewEngine(app)
+initWebRoutes(app)
+
+connectDB()
